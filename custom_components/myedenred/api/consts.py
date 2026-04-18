65d23e6ff0dd77fb7bb30e6443b7e29841aa311b
isMircea <158557806+isMircea@users.noreply.github.com>
2026-04-18 13:12:49 +0300

    Update consts.py

--- custom_components/myedenred/api/consts.py
+++ custom_components/myedenred/api/consts.py
@@ -5,7 +5,7 @@ COUNTRIES = [
 ]
 
 API_LOGIN_URL = {
-    "RO": "https://myedenred.ro/edenred-customer/api/authenticate/default"
+    "RO": "https://sso.eu.edenred.io/web/session/step/password?returnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fresponse_type%3Dcode%26client_id%3D1ea2d5a62e8347adb53f83fc7f3227db%26scope%3Dopenid%2520edg-xp-appcontainer-sa-api%26redirect_uri%3Dhttps%253A%252F%252Fuser.myedenred.ro%252Flogin%26acr_values%3Dtenant%253Aro-ben%26ui_locales%3Dro%26code_challenge%3D3doz2VXczEMNL3E5y0Lv9p-RS-vIK8C0lgU8C0moSjE%26code_challenge_method%3DS256"
 }
 
 API_LIST_URL = {
