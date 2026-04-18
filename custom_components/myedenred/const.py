e1e0484faef8e8a97c930d2256c1ebaa91e55230
isMircea <158557806+isMircea@users.noreply.github.com>
2026-04-18 13:17:17 +0300

    Update const.py

--- custom_components/myedenred/const.py
+++ custom_components/myedenred/const.py
@@ -2,9 +2,9 @@ DOMAIN = "myedenred"
 PLATFORM = "sensor"
 DOMAIN_DATA = f"{DOMAIN}_data"
 
-ATTRIBUTION = "Data provided by https://www.myedenred.pt/"
+ATTRIBUTION = "Data provided by https://www.myedenred.ro/"
 
 DEFAULT_ICON = "mdi:credit-card"
-UNIT_OF_MEASUREMENT = "€"
+UNIT_OF_MEASUREMENT = "Ron"
 
 CONF_COUNTRY = "country"
