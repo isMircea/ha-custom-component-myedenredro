4cc32955fba5717d9fd6a5233cc788a844be69ca
isMircea <158557806+isMircea@users.noreply.github.com>
2026-04-18 12:56:38 +0300

    Update consts.py

--- custom_components/myedenred/api/consts.py
+++ custom_components/myedenred/api/consts.py
@@ -1,21 +1,17 @@
 DEFAULT_COUNTRY = "RO"
 
 COUNTRIES = [
-    { "value": DEFAULT_COUNTRY, "label": "Portugal" },
-    { "value": "RO", "label": "Romania" },
+    { "value": DEFAULT_COUNTRY, "label": "Romania" }
 ]
 
 API_LOGIN_URL = {
-    "PT": "https://www.myedenred.pt/edenred-customer/api/authenticate/default",
     "RO": "https://myedenred.ro/edenred-customer/api/authenticate/default"
 }
 
 API_LIST_URL = {
-    "PT": "https://www.myedenred.pt/edenred-customer/api/protected/card/list",
-    "RO": "https://myedenred.ro/edenred-customer/api/protected/card/list",
+    "RO": "https://myedenred.ro/edenred-customer/api/protected/card/list"
 }
 
 API_ACCOUNTMOVEMENT_URL = {
-    "PT": "https://www.myedenred.pt/edenred-customer/api/protected/card/{}/accountmovement",
-    "RO": "https://myedenred.ro/edenred-customer/api/protected/card/{}/accountmovement",
+    "RO": "https://myedenred.ro/edenred-customer/api/protected/card/{}/accountmovement"
 }
